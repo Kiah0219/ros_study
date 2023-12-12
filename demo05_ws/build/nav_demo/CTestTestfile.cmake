c39e3f5fc929266988b645e870f31aa69f3ecc89
Kiah0219 <117653362+Kiah0219@users.noreply.github.com>
2023-12-12 17:34:21 +0800

    Delete demo05_ws directory

--- demo05_ws/build/nav_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/koy/demo05_ws/src/nav_demo
-# Build directory: /home/koy/demo05_ws/build/nav_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- demo05_ws/build/urdf02_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/koy/demo05_ws/src/urdf02_gazebo
-# Build directory: /home/koy/demo05_ws/build/urdf02_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
